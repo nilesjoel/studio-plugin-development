2299c54bb60b42b3de2c7d0352c6ce4907bc8c8d
nilesjoel <nilesjoel@gmail.com>
2023-05-08 16:45:13 -0500

    Update

---
+++ config/plugins.js
@@ -0,0 +1,30 @@
+module.exports = ({ env }) => ({
+    // ...
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+    },
+    email: {
+      config: {
+        provider: 'sendgrid',
+        providerOptions: {
+          apiKey: env('SENDGRID_API_KEY'),
+        },
+        settings: {
+          defaultFrom: env('EMAIL_DEFAULT_FROM'),
+          defaultReplyTo: env('EMAIL_DEFAULT_REPLY_TO'),
+        },
+      },
+    }
+    // ...
+  })
